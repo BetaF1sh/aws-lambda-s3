2a447257b49f677c137953938106cfe0bcd592fb
Muhun Kim <iam@muhun.kim>
2019-07-10 13:23:10 +0900

    use json format

--- lambda_function.py
+++ lambda_function.py
@@ -1,18 +1,25 @@
 from datetime import datetime
 import boto3
-import yaml
+import json
 from jejunuMeals import JejunuMeals
-
-noalias = yaml.dumper.SafeDumper
-noalias.ignore_aliases = lambda self, data: True
 
 Bucket = boto3.resource('s3').Bucket('jejunu.muhun.dev')
 
 
 def lambda_handler(event, context):
-    Dump = yaml.dump(JejunuMeals().menus(), default_flow_style=False,
-                     allow_unicode=True, Dumper=noalias)
+    Dump = json.dumps(JejunuMeals().menus(), ensure_ascii=False)
     Bucket.put_object(
-        Key='meals/index.yaml', Body=Dump)
+        Key='meals/index.json', Body=Dump)
     Bucket.put_object(
-        Key=f'meals/logs/{datetime.now():%m-%d}.yaml', Body=Dump)
+       Key=f'meals/logs/{datetime.now():%m-%d}.json', Body=Dump)
+
+# TODO: '없음' 들어있는 속성 없애기
+# def lambda_handler():
+#     meals = JejunuMeals().menus()
+#     meals_copy = copy.deepcopy(meals)
+#     for w in meals.keys():
+#         for tim in meals[w].keys():
+#             for typ, val in meals[w][tim].items():
+#                 if val[0] == '없음' or val[0].find(' ') != -1:
+#                     del meals_copy[w][tim][typ]
+#     return meals_copy
